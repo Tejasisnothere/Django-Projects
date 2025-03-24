aa194e51ee7aa58aad363bf6d51772591ef2539f
Tejasisnothere <tejaskadam209@gmail.com>
2025-03-24 22:37:04 +0530

    added procfile and pipfile

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-nlv1#&5@8)5%jv_yu9k*(m1fu63sydeyxu#apte41tqtt!-c6n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
