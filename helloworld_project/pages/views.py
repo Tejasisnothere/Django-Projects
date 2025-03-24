dec1d7635e2ee432f293e3fd4c7d500d7b226838
Tejasisnothere <tejaskadam209@gmail.com>
2025-03-24 18:46:20 +0530

    ignored venv folder

--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here.f
+
+def homePageView(request):
+    return HttpResponse("Hello, World!")
+
